145f7313d78f0ed4268e56f961e6013f7469bf65
RyanVanWolde <122929829+RyanVanWolde@users.noreply.github.com>
2024-02-04 19:53:55 -0500

    Update ServerGraph.cs

--- ServerGraph.cs
+++ ServerGraph.cs
@@ -1,285 +1,279 @@
-﻿using System.Reflection.Metadata.Ecma335;
+using System.Reflection.Metadata.Ecma335;
 using System.Xml.Linq;
+
 namespace _3020_assn1
 {
-public class ServerGraph
-{
-    // 3 marks
-    private class WebServer
+    public class ServerGraph
     {
-        public string Name;
-        public List<WebPage> P;
-        
-        public WebServer(string name, List<WebPage> p)
+        // 3 marks
+        private class WebServer
         {
-            Name = name;
-            P = p;
-        }
-    }
-    private WebServer[] V;
-    private bool[,] E;
-    private int NumServers;
+            public string Name;
+            public List<WebPage> P;
 
-    // 2 marks
-    // Create an empty server graph
-    public ServerGraph()
+            public WebServer(string name, List<WebPage> p)
+            {
+                Name = name;
+                P = p;
+            }
+        }
+        private WebServer[] V;
+        private bool[,] E;
+        private int NumServers;
+
+        // 2 marks
+        // Create an empty server graph
+        public ServerGraph()
         {
             NumServers = 0;
             V = new WebServer[5];
             E = new bool[5, 5];
         }
-    // 2 marks
-    // Return the index of the server with the given name; otherwise return -1
-    private int FindServer(string name)
+        // 2 marks
+        // Return the index of the server with the given name; otherwise return -1
+        private int FindServer(string name)
         {
             for (int i = 0; i < NumServers; i++)
             {
-                if (V[i].Equals(name))
+                if (V[i].Name.Equals(name))
                     return i;
             }
             return -1;
         }
-    // 3 marks
-    // Double the capacity of the server graph with the respect to web servers
-    private void DoubleCapacity()
+        // 3 marks
+        // Double the capacity of the server graph with the respect to web servers
+        private void DoubleCapacity()
         {
             WebServer[] tempV = new WebServer[V.Length * 2];
-            bool [,] tempE = new bool[V.Length*2 , V.Length*2];
-    }
-    // 3 marks
-    // Add a server with the given name and connect it to the other server
-    // Return true if successful; otherwise return false
-    public bool AddServer(string name, string other)
-    {
-        if(NumServers == V.Length)
-        {
-            DoubleCapacity();
-        }   
-        if (FindServer(name) == -1)
-        {
-            V[NumServers] = new WebServer(name, new List<WebPage>());
-            for (int i = 0; i <= NumServers; i++)
-            {
-                E[i, NumServers] = false;
-                E[NumServers, i] = false;
-            }
-            NumServers++;
-            return true;
+            bool[,] tempE = new bool[V.Length * 2, V.Length * 2];
         }
-        return false;
-    }
-    // 3 marks
-    // Add a webpage to the server with the given name
-    // Return true if successful; otherwise return false
-    public bool AddWebPage(WebPage w, string name)
-    {
-        int serverIndex = FindServer(name);
-
-        if (serverIndex > -1)
+        // 3 marks
+        // Add a server with the given name and connect it to the other server
+        // Return true if successful; otherwise return false
+        public bool AddServer(string name, string other)
         {
-            for(int i = 0; i < V[serverIndex].P.Count; i++)
+            if (NumServers == V.Length)
             {
-                if (V[serverIndex].P[i] == w) return false;
+                DoubleCapacity();
             }
-
-            V[serverIndex].P.Add(w);
-            return true;
-        }
-
-        return false;
-
-    }
-    // 4 marks
-    // Remove the server with the given name by assigning its connections
-    // and webpages to the other server
-    // Return true if successful; otherwise return false
-    public bool RemoveServer(string name, string other)
-    {
-        int i, j;
-        if ((i = FindServer(name)) > -1)
-        {
-            NumServers--;
-            V[i] = V[NumServers];
-            for (j = NumServers; j >= 0; j--)
+            if (FindServer(name) == -1)
             {
-                E[j, i] = E[j, NumServers];
-                E[i, j] = E[NumServers, j];
-            }
-            return true;
-        }
-
-        return false;
-    }
-    // 3 marks (Bonus)
-    // Remove the webpage from the server with the given name
-    // Return true if successful; otherwise return false
-    public bool RemoveWebPage(string webpage, string name)
-    {
-        int serverIndex = FindServer(name);
-
-        if (serverIndex > -1)
-        {
-            for (int i = 0; i < V[serverIndex].P.Count; i++)
-            {
-                if (V[serverIndex].P[i].Name == webpage)
+                V[NumServers] = new WebServer(name, new List<WebPage>());
+                for (int i = 0; i <= NumServers; i++)
                 {
-                    V[serverIndex].P.Remove(V[serverIndex].P[i]);
+                    E[i, NumServers] = false;
+                    E[NumServers, i] = false;
+                }
+                NumServers++;
+                return true;
+            }
+            return false;
+        }
+        // 3 marks
+        // Add a webpage to the server with the given name
+        // Return true if successful; otherwise return false
+        public bool AddWebPage(WebPage w, string name)
+        {
+            int serverIndex = FindServer(name);
+
+            if (serverIndex > -1)
+            {
+                for (int i = 0; i < V[serverIndex].P.Count; i++)
+                {
+                    if (V[serverIndex].P[i] == w) return false;
+                }
+
+                V[serverIndex].P.Add(w);
+                return true;
+            }
+
+            return false;
+
+        }
+        // 4 marks
+        // Remove the server with the given name by assigning its connections
+        // and webpages to the other server
+        // Return true if successful; otherwise return false
+        public bool RemoveServer(string name, string other)
+        {
+            int i, j;
+            if ((i = FindServer(name)) > -1)
+            {
+                NumServers--;
+                V[i] = V[NumServers];
+                for (j = NumServers; j >= 0; j--)
+                {
+                    E[j, i] = E[j, NumServers];
+                    E[i, j] = E[NumServers, j];
+                }
+                return true;
+            }
+
+            return false;
+        }
+        // 3 marks (Bonus)
+        // Remove the webpage from the server with the given name
+        // Return true if successful; otherwise return false
+        public bool RemoveWebPage(string webpage, string name)
+        {
+            int serverIndex = FindServer(name);
+
+            if (serverIndex > -1)
+            {
+                for (int i = 0; i < V[serverIndex].P.Count; i++)
+                {
+                    if (V[serverIndex].P[i].Name == webpage)
+                    {
+                        V[serverIndex].P.Remove(V[serverIndex].P[i]);
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            return false;
+        }
+        // 3 marks
+        // Add a connection from one server to another
+        // Return true if successful; otherwise return false
+        // Note that each server is connected to at least one other server
+        public bool AddConnection(string from, string to)
+        {
+            int i, j;
+            if ((i = FindServer(from)) > -1 && (j = FindServer(to)) > -1)
+            {
+                if (E[i, j] == false)
+                {
+                    E[i, j] = true;
+                    E[j, i] = true;
                     return true;
                 }
             }
             return false;
-        }
 
-        return false;
-    }
-    // 3 marks
-    // Add a connection from one server to another
-    // Return true if successful; otherwise return false
-    // Note that each server is connected to at least one other server
-    public bool AddConnection(string from, string to)
-    {
-        int i, j;
-       if ((i = FindServer(from)) > -1 && (j = FindServer(to)) > -1)
-       {
-            if (E[i, j] == false)
-            {
-                E[i, j] = true;
-                return true;
-            }
         }
-        return false;
-           
-    }
-    // 10 marks
-    // Return all servers that would disconnect the server graph into
-    // two or more disjoint graphs if ever one of them would go down
-    // Hint: Use a variation of the depth-first search
-    public string[] CriticalServers()
-    {
-        string[] critServers = new string[NumServers];
-        
-        //loop over all servers and test the connectivity of the graph by checking how many servers are visited 
-        for (int i = 1; i <= NumServers; i++)
+        // 10 marks
+        // Return all servers that would disconnect the server graph into
+        // two or more disjoint graphs if ever one of them would go down
+        // Hint: Use a variation of the depth-first search
+        public string[] CriticalServers()
         {
-            // tally the number of parts of the graph
-            int components = 0;
-            // to track the visited servers
-            bool[] visited = new bool[NumServers + 1];
+            string[] critServers = new string[NumServers];
 
-            // iterate over the broken up graph
-            for (int j = 1; j <= NumServers; j++)
+            //loop over all servers and test the connectivity of the graph by checking how many servers are visited 
+            for (int i = 0; i < NumServers; i++)
             {
-                if (j != i)
+                // tally the number of parts of the graph
+                int components = 0;
+                // to track the visited servers
+                bool[] visited = new bool[NumServers + 1];
+                visited[i] = true;
+
+                // iterate over the broken up graph
+                for (int j = 0; j < NumServers; j++)
                 {
-                    // if server j is not visited form a new component.
-                    if (visited[j] == false)
+                    if (j != i)
                     {
-                        components++;
+                        // if server j is not visited form a new component.
+                        if (visited[j] == false)
+                        {
+                            components++;
 
-                        // call depthfirstsearch to visit all servers connected to the components
-                        DepthFirstSearch(j, visited);
+                            // call depthfirstsearch to visit all servers connected to the components
+                            DepthFirstSearch(j, visited);
+                        }
                     }
+                }
+
+                // If the number of components is more than 1
+                // after removing the ith vertex, then vertex i
+                // is an articulation point.  
+                if (components > 1)
+                {
+                    critServers[i] = V[i].Name;
                 }
             }
 
-            // If the number of components is more than 1
-            // after removing the ith vertex, then vertex i
-            // is an articulation point.  
-            if (components > 1)
+            return critServers;
+        }
+
+
+
+        private void DepthFirstSearch(int i, bool[] visited)
+        {
+            int j;
+
+            visited[i] = true;    // Output vertex when marked as visited
+            //Console.WriteLine(i);
+
+            for (j = 0; j < NumServers; j++)    // Visit next unvisited adjacent vertex
+                if (!visited[j] && E[i, j] == true)
+                    DepthFirstSearch(j, visited);
+        }
+
+        // 6 marks
+        // Return the shortest path from one server to another
+        // Hint: Use a variation of the breadth-first search
+        public int ShortestPath(string from, string to)
+        {
+            Queue<int> Q = new Queue<int>();
+            bool[] visited = new bool[NumServers];
+            int[] distance = new int[NumServers];
+            int[] predecessor = new int[NumServers];
+            int fromIndex = FindServer(from);
+            int toIndex = FindServer(to);
+
+            for (int i = 0; i < NumServers; i++)
             {
-                critServers[i] = V[i].Name;
+                visited[i] = false;
+                distance[i] = int.MaxValue;
+                predecessor[i] = -1;
             }
-        }
 
-        return critServers;
-    }
+            visited[fromIndex] = true;
+            distance[fromIndex] = 0;
+            Q.Enqueue(fromIndex);
 
-    
+            while (Q.Count != 0)
+            {
+                int i = Q.Dequeue();
 
-    private void DepthFirstSearch(int i, bool[] visited)
-    {
-        int j;
+                for (int j = 0; j < NumServers; j++)    // Enqueue unvisited adjacent vertices
+                    if (!visited[j] && E[i, j] == true)
+                    {
+                        // Mark vertex as visited
+                        visited[j] = true;
+                        //set the distance from the start to the current server
+                        distance[j] = distance[i] + 1;
+                        //set the predecessor to the last visited server
+                        predecessor[j] = i;
+                        //queue up the adjacent servers
+                        Q.Enqueue(j);
 
-        visited[i] = true;    // Output vertex when marked as visited
-        Console.WriteLine(i);
-
-        for (j = 0; j < NumServers; j++)    // Visit next unvisited adjacent vertex
-            if (!visited[j] && E[i, j] == true)
-                DepthFirstSearch(j, visited);
-    }
-
-    // 6 marks
-    // Return the shortest path from one server to another
-    // Hint: Use a variation of the breadth-first search
-    public int ShortestPath(string from, string to)
-    {
-        Queue<int> Q = new Queue<int>();
-        bool[] visited = new bool[NumServers];
-        int[] distance = new int[NumServers];
-        int[] predecessor = new int[NumServers];
-        int fromIndex = FindServer(from);
-        int toIndex = FindServer(to);
-
-        for (int i = 0; i < NumServers; i++)
-        {
-            visited[i] = false;
-            distance[i] = int.MaxValue;
-            predecessor[i] = -1;
-        }
-
-        visited[fromIndex] = true;
-        distance[fromIndex] = 0;
-        Q.Enqueue(fromIndex);
-
-        while (Q.Count != 0)
-        {
-            int i = Q.Dequeue();
-
-            for (int j = 0; j < NumServers; j++)    // Enqueue unvisited adjacent vertices
-                if (!visited[j] && E[i, j] == true)  
-                {
-                    // Mark vertex as visited
-                    visited[j] = true;   
-                    //set the distance from the start to the current server
-                    distance[j] = distance[i] + 1;
-                    //set the predecessor to the last visited server
-                    predecessor[j] = i;
-                    //queue up the adjacent servers
-                    Q.Enqueue(j);
-
-                    //if reached the destination then the shortest path has been found
-                    if (j == toIndex) return distance[j];
+                        //if reached the destination then the shortest path has been found
+                        if (j == toIndex) return distance[j];
                     }
             }
 
-        return -1;
-    }
-    // 4 marks
-    // Print the name and connections of each server as well as
-    // the names of the webpages it hosts
-    public void PrintGraph()
-    {
-            Console.WriteLine("ServerGraph:");
-        //print all the servers
-        int i;
-        for (i = 0; i < NumServers; i++)
-            Console.WriteLine(V[i].Name);    //@TODO: write all the associated web pages as well
-
-        //print all the connections
-        int j;
+            return -1;
+        }
+        // 4 marks
+        // Print the name and connections of each server as well as
+        // the names of the webpages it hosts
+        public void PrintGraph()
+        {
+            //print all the servers
+            int i;
             for (i = 0; i < NumServers; i++)
-            {
+                Console.WriteLine(V[i].Name);    //@TODO: write all the associated web pages as well
+
+            //print all the connections
+            int j;
+            for (i = 0; i < NumServers; i++)
                 for (j = 0; j < NumServers; j++)
-                {
                     if (E[i, j] == true)
-                        Console.WriteLine("Connection: (" + V[i].Name + "," + V[j].Name + ")");
-                }
-            }
-            Console.WriteLine("\n\n\n\n\n");
+                        Console.WriteLine("(Connection: " + V[i].Name + "," + V[j].Name + ")");
+        }
+
     }
-
-    
 }
-
-}
-
